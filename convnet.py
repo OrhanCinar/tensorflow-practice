34be60c28300cf27befc3fcf8876943fbe7cda95
Orhan CINAR <orhancinar35@gmail.com>
2019-10-11 02:34:03 +0200

    add tensorboard

--- convnet.py
+++ convnet.py
@@ -1,7 +1,18 @@
 import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, DropOut, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.callback import TensorBoard
 import pickle
+import time
+
+NAME = f"Cats-vs-dog-cc-64x2-{int(time.time())}"
+
+tensorboard = TensorBoard(log_dir=f"logs/{NAME}")
+
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 x = pickle.load(open("x.pickle", "rb"))
 y = pickle.load(open("y.pickle", "rb"))
@@ -17,7 +28,9 @@ model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
+
 model.add(Dense(64))
+model.add(Activation("relu"))
 
 model.add(Dense(1))
 model.add(Activation("sigmoid"))
@@ -25,4 +38,5 @@ model.add(Activation("sigmoid"))
 model.compile(loss="binary_crossentropy",
               optimizer="adam", metrics=["accuracy"])
 
-model.fit(x, y, batch_size=32, epochs=3, validation_split=0.1)
+model.fit(x, y, batch_size=32, epochs=10,
+          validation_split=0.1, callbacks=[tensorboard])
